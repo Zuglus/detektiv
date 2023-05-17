9da66d01d9c3b8788e96b9db532f07b69603a6c9
Andrey Kynterets <an@kynterets.ru>
2023-05-17 09:40:57 +0400

    Add breadcrumbs and correcting routes

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,4 +1,8 @@
-export default function Card({ data }) {
+export default function Card({
+  data,
+}: {
+  data: { title: string; text: string };
+}) {
   return (
     <div className='text-center p-6 border-2 border-solid border-gray-800'>
       <h4 className='uppercase font-bold text-lg'>{data.title}</h4>
