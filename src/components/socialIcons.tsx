7f9accb37815b14930bb94c148eb1530fc4a63ec
Andrey Kynterets <an@kynterets.ru>
2023-06-05 23:47:24 +0400

    added social icons

---
+++ src/components/socialIcons.tsx
@@ -0,0 +1,26 @@
+import { email, phone, telegram, whatsapp } from '@/data/contacts.json';
+
+export default function SocialIcons() {
+  return (
+    <div className='hidden md:flex'>
+      {[phone, email, telegram, whatsapp].map((icon) => {
+        return (
+          <a href={icon.link} key={icon.name} className='my-auto ml-2'>
+            <svg
+              height={25}
+              width={25}
+              fill-rule='evenodd'
+              stroke-linejoin='round'
+              stroke-miterlimit='1.414'
+              clip-rule='evenodd'
+              viewBox='0 0 24 24'
+              fill='red'
+            >
+              <path d={icon.icon} />
+            </svg>
+          </a>
+        );
+      })}
+    </div>
+  );
+}
