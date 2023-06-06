2270a5bc579cb10329498b32c1983f36571552ea
Andrey Kynterets <an@kynterets.ru>
2023-06-06 23:02:39 +0400

    styled vakansii

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -4,7 +4,7 @@ export default function Card({
   data: { title: string; text: string };
 }) {
   return (
-    <div className='text-center p-6 border-2 border-solid border-gray-800'>
+    <div className='text-center p-6 border-2 border-solid border-gray-800 my-10 md:w-1/2'>
       <h4 className='uppercase font-bold text-lg'>{data.title}</h4>
       <div className='mt-4 text-sm'>{data.text}</div>
     </div>
