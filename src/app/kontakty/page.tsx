24f6e0040834b0934f5c4290302b4121111885e7
Andrey Kynterets <an@kynterets.ru>
2023-06-07 00:20:27 +0400

    styled kontakty

--- src/app/kontakty/page.tsx
+++ src/app/kontakty/page.tsx
@@ -15,61 +15,61 @@ export default function Contact() {
   return (
     <>
       <Breadcrumbs name='Контакты' link={''} secondName={''} />
-      <section className='mb-32 text-center'>
-        <div className='max-w-[700px] mx-auto px-3 lg:px-6'>
-          <h2 className='text-3xl font-bold mb-12'>Контакты</h2>
-          <div className='flex'>
-            <ul>
-              <li>
-                <div>Официальный и единственный адрес:</div>
-                <div>{address}</div>
-              </li>
-              <li>
-                <div>E-mail:</div>
-                <a href={email.link}>{email.name}</a>
-              </li>
-              <li>
-                <div>Мобильный:</div>
-                <a href={phone.link}>{phone.name}</a>
-              </li>
-              <li>
-                <div>Telegram (нажать иконку):</div>
-                <a href={telegram.link}>
-                  <svg
-                    className='m-auto'
-                    height={40}
-                    width={40}
-                    fill-rule='evenodd'
-                    stroke-linejoin='round'
-                    stroke-miterlimit='1.414'
-                    clip-rule='evenodd'
-                    viewBox='0 0 24 24'
-                    fill='white'
-                  >
-                    <path d={telegram.icon} />
-                  </svg>
-                </a>
-              </li>
-              <li>
-                <div>Whatsapp (нажать иконку):</div>
-                <a href={whatsapp.link}>
-                  <svg
-                    className='m-auto'
-                    height={40}
-                    width={40}
-                    fill-rule='evenodd'
-                    stroke-linejoin='round'
-                    stroke-miterlimit='1.414'
-                    clip-rule='evenodd'
-                    viewBox='0 0 24 24'
-                    fill='white'
-                  >
-                    <path d={whatsapp.icon} />
-                  </svg>
-                </a>
-              </li>
-            </ul>
-          </div>
+      <section className='md:w-full mb-12 text-center'>
+        <div className='px-3 lg:px-6'>
+          <h2 className='text-3xl font-bold mb-10'>Контакты</h2>
+          <ul>
+            <li className='bg-neutral-900 p-3 mb-5 md:w-1/2 mx-auto'>
+              <div className='text-neutral-400'>
+                Официальный и единственный адрес:
+              </div>
+              <div>{address}</div>
+            </li>
+            <li className='bg-neutral-900 p-3 mb-5 md:w-1/2 mx-auto'>
+              <div className='text-neutral-400'>E-mail:</div>
+              <a href={email.link}>{email.directName}</a>
+            </li>
+            <li className='bg-neutral-900 p-3 mb-5 md:w-1/2 mx-auto'>
+              <div className='text-neutral-400'>Мобильный:</div>
+              <a href={phone.link}>{phone.name}</a>
+            </li>
+            <li className='bg-neutral-900 p-3 mb-5 md:w-1/2 mx-auto'>
+              <div className='text-neutral-400'>Telegram (нажать иконку):</div>
+              <a href={telegram.link}>
+                <svg
+                  className='m-auto'
+                  height={40}
+                  width={40}
+                  fill-rule='evenodd'
+                  stroke-linejoin='round'
+                  stroke-miterlimit='1.414'
+                  clip-rule='evenodd'
+                  viewBox='0 0 24 24'
+                  fill='white'
+                >
+                  <path d={telegram.icon} />
+                </svg>
+              </a>
+            </li>
+            <li className='bg-neutral-900 p-3 md:w-1/2 mx-auto'>
+              <div className='text-neutral-400'>Whatsapp (нажать иконку):</div>
+              <a href={whatsapp.link}>
+                <svg
+                  className='m-auto'
+                  height={40}
+                  width={40}
+                  fill-rule='evenodd'
+                  stroke-linejoin='round'
+                  stroke-miterlimit='1.414'
+                  clip-rule='evenodd'
+                  viewBox='0 0 24 24'
+                  fill='white'
+                >
+                  <path d={whatsapp.icon} />
+                </svg>
+              </a>
+            </li>
+          </ul>
         </div>
       </section>
     </>
