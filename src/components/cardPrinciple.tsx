43a9621719311a1dd4c88cdd213e63dffc4d89f5
Andrey Kynterets <an@kynterets.ru>
2023-06-07 12:32:49 +0400

    added component CardPrinciple

---
+++ src/components/cardPrinciple.tsx
@@ -0,0 +1,12 @@
+export default function CardPrinciple({
+  data,
+}: {
+  data: { title: string; text: string };
+}) {
+  return (
+    <div className='text-center p-6 border-2 border-solid border-gray-800'>
+      <h4 className='uppercase font-bold text-lg'>{data.title}</h4>
+      <div className='mt-4 text-sm'>{data.text}</div>
+    </div>
+  );
+}
