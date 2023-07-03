e21c77772e3e9e0d1d84bc1c28f3d2a28e345fcb
Andrey Kynterets <an@kynterets.ru>
2023-07-03 17:58:57 +0400

    changed guarantee component

--- src/components/buttonTranslate.tsx
+++ src/components/buttonTranslate.tsx
@@ -7,7 +7,7 @@ import TranslateUrl from "./translateUrl";
 export default function ButtonTranslate({ url, isOpen, flag }: { url: string, isOpen: boolean, flag: boolean }) {
   const trans = TranslateUrl(url);
   return (
-    <Link className={classNames(isOpen ? 'hidden' : '', 'my-auto')} href={trans.link || '/'}>{trans?.flag ? <svg xmlns="http://www.w3.org/2000/svg" height={20} viewBox="0 0 640 480">
+    <Link className={classNames(isOpen ? 'hidden' : '', 'my-auto')} href={trans.link || '/'}>{trans?.flag ? <svg xmlns="http://www.w3.org/2000/svg" height={15} viewBox="0 0 640 480">
       <path fill="#bd3d44" d="M0 0h640v480H0" />
       <path stroke="#fff" stroke-width="37" d="M0 55.3h640M0 129h640M0 203h640M0 277h640M0 351h640M0 425h640" />
       <path fill="#192f5d" d="M0 0h364.8v258.5H0" />
@@ -15,7 +15,7 @@ export default function ButtonTranslate({ url, isOpen, flag }: { url: string, is
         <path fill="#fff" d="m14 0 9 27L0 10h28L5 27z" />
       </marker>
       <path fill="none" marker-mid="url(#a)" d="m0 0 16 11h61 61 61 61 60L47 37h61 61 60 61L16 63h61 61 61 61 60L47 89h61 61 60 61L16 115h61 61 61 61 60L47 141h61 61 60 61L16 166h61 61 61 61 60L47 192h61 61 60 61L16 218h61 61 61 61 60L0 0" />
-    </svg> : <svg xmlns="http://www.w3.org/2000/svg" height={20} viewBox="0 0 640 480">
+    </svg> : <svg xmlns="http://www.w3.org/2000/svg" height={15} viewBox="0 0 640 480">
       <g fill-rule="evenodd" stroke-width="1pt">
         <path fill="#fff" d="M0 0h640v480H0z" />
         <path fill="#0039a6" d="M0 160h640v320H0z" />
