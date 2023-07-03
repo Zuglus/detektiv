6c2d60af26f98e4e6eba636e61776626d79ff13c
Andrey Kynterets <an@kynterets.ru>
2023-07-03 15:52:34 +0400

    fixed types in buttonTranslate

--- src/app/(ru)/layout.tsx
+++ src/app/(ru)/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: Props) {
     <html lang='ru'>
       <body className='font-sans antialiased p-4 selection:bg-red-600 selection:text-black'>
         <div className='container mx-auto'>
-          <Head data={headData.ru} routes={routes.ru} routesToTrans={routes.en} />
+          <Head data={headData.ru} routes={routes.ru} />
           <main className='flex flex-col items-center justify-between'>
             {children}
           </main>
