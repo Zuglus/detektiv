93b63fb1b80810ce1df01f9df3e22f20b8c85cee
Andrey Kynterets <an@kynterets.ru>
2023-09-18 14:27:40 +0400

    deleted price numbers

--- src/components/cardService.tsx
+++ src/components/cardService.tsx
@@ -6,7 +6,7 @@ export default function CardService({
   return (
     <div className='text-center p-6 border-2 border-solid border-gray-800 hover:animate-pulse'>
       <h4 className='text-red-500 uppercase font-bold text-lg'>{data.title}</h4>
-      <div className='mt-4 text-sm'>{data.price}</div>
+      {/* <div className='mt-4 text-sm'>{data.price}</div> */}
       <div className='mt-4 text-neutral-400 font-light'>{data.text}</div>
     </div>
   );
