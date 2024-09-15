7009bb7ca290a0f5260902cc0f4fc6a4e74cf883
Zuglus <kynterets@gmail.com>
2024-09-15 22:59:11 +0400

    изменена en/about/page

--- src/app/(ru)/page.tsx
+++ src/app/(ru)/page.tsx
@@ -1,13 +1,10 @@
 import ContentMain from '@/components/content/contentMain';
 import contentData from '@/data/main.json';
 import contactsData from '@/data/contacts.json';
-import {ContentData, Contacts} from '@/components/utility/types'
 
 export default function About() {
 
   return (
-    <>
-      <ContentMain data={contentData.ru} contacts={contactsData} />
-    </>
+    <ContentMain data={contentData.ru} contacts={contactsData} />
   );
 }
