a281df877f9ed949bdba7c42eda35bfbc9c59fcb
Zuglus <kynterets@gmail.com>
2024-09-18 10:36:15 +0400

    сделан перевод guarantee

--- src/app/(ru)/garantii/page.tsx
+++ src/app/(ru)/garantii/page.tsx
@@ -1,7 +1,5 @@
 import { Breadcrumbs } from '@/components/layout/breadcrumbs';
-import ContentGuarantee from '@/components/content/contentGuarantee';
-import contacts from '@/data/contacts.json';
-import content from '@/data/guarantee.json';
+import ContentGuarantee from '@/components/content/guarantee/contentGuarantee';
 import { Breadcrumb } from '@/components/utility/types';
 
 export const metadata = {
@@ -18,7 +16,7 @@ export default function Garanty() {
   return (
     <>
       <Breadcrumbs breadcrumb={breadcrumb} />
-      <ContentGuarantee contacts={contacts} data={content.ru} />
+      <ContentGuarantee lang='ru' />
     </>
   );
 }
--- src/components/content/guarantee/contentGuarantee.tsx
+++ src/components/content/guarantee/contentGuarantee.tsx
@@ -1,30 +1,40 @@
-export default function ContentGuarantee({ data, contacts }: { data: any, contacts: any }) {
+import { Lang } from "@/components/utility/types";
+import content from './guarantee.json';
+import contacts from '@/data/contacts.json';
+
+export default function ContentGuarantee({lang}: {lang: Lang}) {
+  const {header,
+    subheader,
+    text,
+    boxText,
+    propose
+  } = content;
   return (
     <>
       {/* Header Section */}
       <h3 className="py-6 font-extrabold text-[#2a4f4f] text-2xl tracking-wide">
-        {data.header}
+        {header[lang]}
       </h3>
 
       {/* Subheader */}
       <p className="mb-6 text-lg leading-relaxed">
-        {data.subheader}
+        {subheader[lang]}
       </p>
 
       {/* Main Content with Shadow and Padding */}
       <p
-        className="bg-white shadow-[#50c878] shadow-lg p-6 rounded-lg text-gray-800 leading-loose"
-        dangerouslySetInnerHTML={{ __html: data.text }}
+        className="bg-[#f4f4f4] shadow-lg p-6 rounded-lg max-w-prose font-serif text-[#333] leading-loose"
+        dangerouslySetInnerHTML={{ __html: text[lang] }}
       />
 
       {/* Boxed Highlight Section */}
       <p className="bg-[#a8d0b9] shadow-md mt-6 p-4 rounded-lg font-semibold text-[#2a4f4f] text-lg">
-        {data.boxText}
+        {boxText[lang]}
       </p>
 
       {/* Contact Proposal Section */}
       <p className='py-10'>
-        {data.propose}
+        {propose[lang]}
         {[
           contacts.telegram,
           contacts.whatsapp,
