62f9cc529a0775e1d7616a4ec5130efa93f324c0
Zuglus <kynterets@gmail.com>
2024-09-18 10:36:15 +0400

    сделан перевод guarantee

--- src/components/content/about/contentAbout.tsx
+++ src/components/content/about/contentAbout.tsx
@@ -5,7 +5,7 @@ export default function ContentAbout({ lang }: ContentLang) {
   const { title, paragraphs } = content;
 
   return (
-    <div className="bg-[#f4f4f4] shadow-lg mx-auto mt-12 p-6 md:p-10 rounded-lg max-w-prose font-serif text-black">
+    <div className="bg-[#f4f4f4] shadow-lg mx-auto mt-12 p-6 md:p-10 rounded-lg max-w-prose font-serif text-[#333]">
       <h3 className="mb-6 font-extrabold text-3xl text-center text-gray-700 uppercase tracking-widest">
         {title[lang]}
       </h3>
