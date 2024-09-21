28a11431a5ca6140f1bd46ec8e61edefedb6c81c
Zuglus <kynterets@gmail.com>
2024-09-21 08:35:18 +0400

    изменен contentMain и соответствующий файл json

--- src/app/(en)/en/page.tsx
+++ src/app/(en)/en/page.tsx
@@ -1,11 +1,9 @@
-import ContentMain from '@/components/content/contentMain';
-import content from '@/data/main.json';
-import contacts from '@/data/contacts.json';
+import ContentMain from '@/components/content/main/contentMain';
 
 export default function MainEn() {
   return (
     <>
-      <ContentMain data={content.en} contacts={contacts} />
+      <ContentMain lang="en" />
     </>
   );
 }
