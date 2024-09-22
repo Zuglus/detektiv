97cc24092897f22fa45541211c585789a39818d2
Zuglus <kynterets@gmail.com>
2024-09-22 11:01:55 +0400

    доработан дизайн и адаптивность contentContact

--- src/components/content/contact/contentContact.tsx
+++ src/components/content/contact/contentContact.tsx
@@ -11,23 +11,26 @@ export default function ContentContact({
     <section className="mb-12 md:w-full text-center">
       <div className="px-4 lg:px-8">
         {/* Header */}
-        <h2 className="mb-12 font-extrabold text-[#2a4f4f] text-4xl tracking-wider">
+        <h2 className="mb-12 font-extrabold text-[#2a4f4f] text-4xl md:text-5xl tracking-wider transition-transform duration-300 hover:scale-105">
           {content.header[lang]}
         </h2>
+
 
         {/* Contact Information */}
         <ul className="list-none">
           {/* Address */}
-          <li className="bg-[#a8d0b9] hover:bg-[#50c878] shadow-lg mx-auto mb-6 p-4 rounded-lg md:w-1/2 transition-colors duration-300">
-            <div className="mb-2 font-bold text-gray-700 text-lg">
+          <li className="bg-gradient-to-r from-[#50c878] to-[#a8d0b9] shadow-lg mx-auto mb-6 p-6 rounded-lg md:w-2/3 transform transition-transform duration-300 hover:scale-105">
+            <div className="mb-2 font-bold text-[#2a4f4f] text-lg">
               {content.addressTitle[lang]}
             </div>
-            <div className="text-gray-800">{content.address[lang]}</div>
+            <div className="text-gray-800">
+              {content.address[lang]}
+            </div>
           </li>
 
           {/* Email */}
-          <li className="bg-[#a8d0b9] hover:bg-[#50c878] shadow-lg mx-auto mb-6 p-4 rounded-lg md:w-1/2 transition-colors duration-300">
-            <div className="mb-2 font-bold text-gray-700 text-lg">
+          <li className="bg-gradient-to-r from-[#50c878] to-[#a8d0b9] shadow-lg mx-auto mb-6 p-6 rounded-lg md:w-2/3 transform transition-transform duration-300 hover:scale-105">
+            <div className="mb-2 font-bold text-[#2a4f4f] text-lg">
               {content.email[lang]}
             </div>
             <a
@@ -39,8 +42,8 @@ export default function ContentContact({
           </li>
 
           {/* Mobile */}
-          <li className="bg-[#a8d0b9] hover:bg-[#50c878] shadow-lg mx-auto mb-6 p-4 rounded-lg md:w-1/2 transition-colors duration-300">
-            <div className="mb-2 font-bold text-gray-700 text-lg">
+          <li className="bg-gradient-to-r from-[#50c878] to-[#a8d0b9] shadow-lg mx-auto mb-6 p-6 rounded-lg md:w-2/3 transform transition-transform duration-300 hover:scale-105">
+            <div className="mb-2 font-bold text-[#2a4f4f] text-lg">
               {content.mobile[lang]}
             </div>
             <a
@@ -52,15 +55,15 @@ export default function ContentContact({
           </li>
 
           {/* Telegram */}
-          <li className="bg-[#a8d0b9] hover:bg-[#50c878] shadow-lg mx-auto mb-6 p-4 rounded-lg md:w-1/2 transition-colors duration-300">
-            <div className="mb-2 font-bold text-gray-700 text-lg">
+          <li className="bg-gradient-to-r from-[#50c878] to-[#a8d0b9] shadow-lg mx-auto mb-6 p-6 rounded-lg md:w-2/3 transform transition-transform duration-300 hover:scale-105">
+            <div className="mb-2 font-bold text-[#2a4f4f] text-lg">
               {content.telegram[lang]}
             </div>
             <a href={contacts.telegram.link} className="inline-block mx-auto">
               <svg
-                className="mx-auto mt-4 transition-transform hover:scale-110 duration-300"
-                height={40}
-                width={40}
+                className="mx-auto mt-4 transition-transform hover:scale-110 hover:rotate-6 duration-300"
+                height={50}
+                width={50}
                 fillRule="evenodd"
                 strokeLinejoin="round"
                 strokeMiterlimit="1.414"
@@ -74,15 +77,15 @@ export default function ContentContact({
           </li>
 
           {/* WhatsApp */}
-          <li className="bg-[#a8d0b9] hover:bg-[#50c878] shadow-lg mx-auto p-4 rounded-lg md:w-1/2 transition-colors duration-300">
-            <div className="mb-2 font-bold text-gray-700 text-lg">
+          <li className="bg-gradient-to-r from-[#50c878] to-[#a8d0b9] shadow-lg mx-auto mb-6 p-6 rounded-lg md:w-2/3 transform transition-transform duration-300 hover:scale-105">
+            <div className="mb-2 font-bold text-[#2a4f4f] text-lg">
               {content.whatsapp[lang]}
             </div>
             <a href={contacts.whatsapp.link} className="inline-block mx-auto">
               <svg
-                className="mx-auto mt-4 transition-transform hover:scale-110 duration-300"
-                height={40}
-                width={40}
+                className="mx-auto mt-4 transition-transform hover:scale-110 hover:rotate-6 duration-300"
+                height={50}
+                width={50}
                 fillRule="evenodd"
                 strokeLinejoin="round"
                 strokeMiterlimit="1.414"
@@ -96,10 +99,13 @@ export default function ContentContact({
           </li>
         </ul>
 
+
+
         {/* Accord Section */}
-        <div className="bg-[#2a4f4f] shadow-md m-auto mt-8 px-6 py-4 rounded-lg max-w-lg text-white">
+        <div className="bg-[#2a4f4f] shadow-md m-auto mt-8 px-6 py-6 rounded-lg max-w-lg text-lg text-white leading-relaxed">
           {content.accord[lang]}
         </div>
+
       </div>
     </section>
   );
