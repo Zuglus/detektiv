f27218fa04f897089822167f2474df9f43e0a7a8
Zuglus <kynterets@gmail.com>
2026-02-13 20:41:39 +0400

    refactor: move jest config files to config/jest/

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,35 +1,36 @@
 const config = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jsdom',
-  testMatch: ['**/__tests__/**/*.test.(ts|tsx|js|jsx)', '**/*.(test|spec).(ts|tsx|js|jsx)'],
-  transform: {
-    '^.+\\.(ts|tsx)$': '<rootDir>/jest.tsTransformer.cjs',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(marked|gray-matter)/)',
+  setupFilesAfterEnv: ["<rootDir>/config/jest/setup.js"],
+  testEnvironment: "jsdom",
+  testMatch: [
+    "**/__tests__/**/*.test.(ts|tsx|js|jsx)",
+    "**/*.(test|spec).(ts|tsx|js|jsx)",
   ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "<rootDir>/config/jest/tsTransformer.cjs",
+  },
+  transformIgnorePatterns: ["node_modules/(?!(marked|gray-matter)/)"],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '^@/app/globals\\.css$': '<rootDir>/jest.styleMock.js',
-    '\\.(css|scss|sass|less)$': '<rootDir>/jest.styleMock.js',
-    '^next/image$': '<rootDir>/jest.nextImageMock.js',
-    '^next/link$': '<rootDir>/jest.nextLinkMock.js',
-    '^marked$': '<rootDir>/jest.markedMock.js',
-    '^gray-matter$': '<rootDir>/jest.grayMatterMock.js',
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@/app/globals\\.css$": "<rootDir>/config/jest/styleMock.js",
+    "\\.(css|scss|sass|less)$": "<rootDir>/config/jest/styleMock.js",
+    "^next/image$": "<rootDir>/config/jest/nextImageMock.js",
+    "^next/link$": "<rootDir>/config/jest/nextLinkMock.js",
+    "^marked$": "<rootDir>/config/jest/markedMock.js",
+    "^gray-matter$": "<rootDir>/config/jest/grayMatterMock.js",
   },
   testPathIgnorePatterns: [
-    '<rootDir>/.next/',
-    '<rootDir>/node_modules/',
-    '<rootDir>/out/',
-    '<rootDir>/e2e/',
-    '<rootDir>/.jest-dist/',
+    "<rootDir>/.next/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/out/",
+    "<rootDir>/e2e/",
+    "<rootDir>/.jest-dist/",
   ],
   collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/app/**/layout.tsx',
-    '!src/app/**/page.tsx',
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/app/**/layout.tsx",
+    "!src/app/**/page.tsx",
   ],
-  coverageReporters: ['text', 'lcov', 'html'],
-}
-export default config
+  coverageReporters: ["text", "lcov", "html"],
+};
+export default config;
